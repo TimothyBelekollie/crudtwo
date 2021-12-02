35a09ca4f51aa8f148c307b2a9881558e8b72dd5
Timothy Belekollie <belekollietimothy2@gmail.com>
2021-12-02 02:22:46 +0200

    Laravel

---
+++ database/migrations/2021_11_28_204221_create_blog_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog_posts', function (Blueprint $table) {
+            $table->id();
+            //this is the code that I added to completed this table
+            $table->text('title');
+            $table->text('body');
+            $table->text('user_id');
+            //this is where I stop writing the code
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blog_posts');
+    }
+}
